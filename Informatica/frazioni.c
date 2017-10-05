bfb68af56437a33051805b03d28b74ca5c72342d
Your Name <you@example.com>
2017-10-05 10:29:41 +0200

    esercizio frazioni

--- Informatica/frazioni.c
+++ Informatica/frazioni.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+int mcd(int a,int b){
+    while(a!=b){
+        if(a>b) a=a-b;
+        else b=b-a;
+    }
+    if(a==1||b==1) return -1;
+      else return a;
+    }
+
+int main(){
+    int n,d;
+    printf("Inserisci la frazione :");
+    scanf("%d/%d",&n,&d);
+    int min=mcd(n,d);
+    printf("\n%d/%d",n/min,d/min);
+}
