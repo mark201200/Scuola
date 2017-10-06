a6da848c0ac8ad7e0a8a2371a95e3ec3bfa97ea7
Your Name <you@example.com>
2017-10-06 13:34:32 +0200

    Esercizio fraziomi fatto

--- Informatica/frazioni.c
+++ Informatica/frazioni.c
@@ -7,31 +7,68 @@ int mcd(int a,int b){
     if(a==1||b==1) return -1;
       else return a;
     }
-
+int mcm(int a,int b){
+    int a1=a;
+    int b1=b;
+while(a!=b){
+    if(a>b) b=b+b1;
+    else a=a+a1;
+}
+return a;
+}
 void molt(int *a,int *b,int a2, int b2){
 int va=*a;
 int vb=*b;
 *a=va*a2;
 *b=vb*b2;
 }
+void add(int *a,int *b,int a2, int b2){
+int v_mcm=mcm(*b,b2);
+int vb=*b;
+int va=*a;
+*b=v_mcm;
+*a=va*(v_mcm/vb)+a2*(v_mcm/b2);
 
+}
+void sott(int *a,int *b,int a2, int b2){
+int v_mcm=mcm(*b,b2);
+int vb=*b;
+int va=*a;
+*b=v_mcm;
+*a=va*(v_mcm/vb)-a2*(v_mcm/b2);
+}
 int main(){
-    int n,d,n1,d1;
+
+    int n,d,n1,d1,s;
     do{
     printf("Inserisci la frazione n 1 :");
-    scanf("%d/%d",&n,&d);
+    scanf("%d",&n);
+    scanf("%d",&d);
     }while(d==0);
     do{
     printf("Inserisci la frazione n 2 :");
-    scanf("%d/%d",&n1,&d1);
+    scanf("%d",&n1);
+    scanf("%d",&d1);
     }while(d1==0);
-    printf("\nCosa vuoi fare?\n");
-    //Scegli tra semplifica,molt,divis,etc
+    printf("\nCosa vuoi fare?\n1:Moltiplicazione\n2:Divisione\n3:Addizione\n4:Sottrazione");
+    scanf("%d",&s);
+    switch (s){
+    case 1:
+        molt(&n,&d,n1,d1);
+        break;
 
-    /* sta roba per ora non serve, uso la selezione
-    int min=mcd(n,d);
-    printf("\n%d/%d",n/min,d/min);
-    molt(&n,&d,n1,d1);
-    printf("Moltiplicazione:\n%d/%d",n,d);
-    */
+    case 2:
+        molt(&n,&d,d1,n1);
+        break;
+
+    case 3:
+        add(&n,&d,n1,d1);
+        break;
+
+    case 4:
+        sott(&n,&d,n1,d1);
+        break;
+
+    }
+    printf("\n%d/%d",n,d);
 }
