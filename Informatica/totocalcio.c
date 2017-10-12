09f38aa409fa6bc5e4c3e1e8c39087d6bfe697de
marco <alto.marco@gmail.com>
2017-10-12 13:37:22 +0200

    Totocalcio finito

--- Informatica/totocalcio.c
+++ Informatica/totocalcio.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
-/*
-totocalcio:
-
-Verifica se le 4 giocate sono vincenti.
-
-*/
 
 void ins_mat(int a[][14]){
     int i,j;
     for (i=0;i<4;i++){
         for(j=0;j<14;j++){
-            printf("Inserisci il numero %d della giocata %d\n",j,i);
+            printf("Inserisci il numero %d della giocata %d\n",j+1,i+1);
             scanf("%d",&a[i][j]);
         }
     }
 }
 
-void stampa_mat(int a[][14]){
-    int j;
+void ins_vet(int a[]){
+    int i;
+        for(i=0;i<14;i++){
+            printf("Inserisci il numero %d della giocata vincente \n",i+1);
+            scanf("%d",&a[i]);
+        }
+}
 
+void stampa_mat(int a[][14],int b[]){
+    int j;
+    printf("\n\n\n\nVINCENTE\tGiocata 1\tGiocata 2\tGiocata 3\tGiocata 4\n\n");
         for(j=0;j<14;j++){
-            printf("%d %d %d %d",a[0][j],a[1][j],a[2][j],a[3][j]);
+            printf("%d\t\t%d\t\t%d\t\t%d\t\t%d",b[j],a[0][j],a[1][j],a[2][j],a[3][j]);
             printf("\n");
         }
-
-
 }
 
 void check_uguale(int v[],int a[][14])
@@ -38,21 +38,19 @@ void check_uguale(int v[],int a[][14])
             }
         }
     }
-    printf("Le giocate vincenti sono:");
+    printf("\nLe giocate vincenti sono:\n");
     for(i=0;i<4;i++){
-        if(win[i]==1) printf("%d",i);
+        if(win[i]==1) printf("Giocata numero %d\n",i+1);
     }
-
-
 }
-
 
 int main(){
 int toto[4][14];
-int v[14]={1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+int v[14];
+printf("\n\nINSERIMENTO GIOCATA VINCENTE\n\n");
+ins_vet(v);
+printf("\n\nINSERIMENTO GIOCATE\n\n");
 ins_mat(toto);
-stampa_mat(toto);
+stampa_mat(toto,v);
 check_uguale(v,toto);
-
-
 }
