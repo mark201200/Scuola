177060bad0d9aefb6a7dd03a65ef93901c7529f2
Your Name <you@example.com>
2018-01-26 13:35:26 +0100

    Inizio java

--- Informatica/Java/nPrimi/src/com/company/Main.java
+++ Informatica/Java/nPrimi/src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        Scanner ccq = new Scanner(System.in);
+        System.out.println("Inserisci un numero per mostrarne i divisori.");
+        int i = ccq.nextInt(),nd=0;
+        System.out.print("Divisori "+i+" = 1 , ");
+       for(int j=2;j<=i/2;j++){
+           if(i%j==0){
+               System.out.print(j+" , ");
+               nd++;
+           }
+       }
+       System.out.println(i);
+       if(nd==0) System.out.println("Il Numero e' primo.");
     }
 }
