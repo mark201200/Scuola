5349b421b08b483f188a0fac61e39cb504e97c0a
Your Name <you@example.com>
2018-03-15 11:57:34 +0100

    angoli, quasi finito, manca il costruttore e la somma

--- Informatica/Java/Angoli/src/com/company/Main.java
+++ Informatica/Java/Angoli/src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Angolo a=new Angolo(10,20,10);
-        a.aggiungiGradi(10);
+        Angolo a=new Angolo(0,0,0);
+        a.secondiAngolo(119);
         a.visualizzaAngolo();
 
 
