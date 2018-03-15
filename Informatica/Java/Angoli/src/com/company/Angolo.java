2b4022f3a6991c4dbc6467c428719c3b0449561a
Your Name <you@example.com>
2018-03-15 10:52:44 +0100

    angoli

---
+++ Informatica/Java/Angoli/src/com/company/Angolo.java
@@ -0,0 +1,51 @@
+package com.company;
+
+public class Angolo {
+    private int gradi, primi, secondi;
+
+    public Angolo(int g, int p, int s) {
+        gradi = g;
+        primi = p;
+        secondi = s;
+    }
+
+    public void visualizzaAngolo() {
+        System.out.println("Angolo: " + gradi + "°" + primi + "'" + secondi + "''");
+    }
+
+    public void aggiungiGradi(int g) {
+        if (g + gradi < 360) gradi = g + gradi;
+    }
+
+    public void aggiungiPrimi(int p) {
+        if (p + primi < 60) primi = p + secondi;
+    }
+
+    public void aggiungiSecondi(int s) {
+        if (s + secondi < 60) secondi = s + secondi;
+    }
+
+    public void secondiAngolo(int s) {
+        int p = Math.floorDiv(s, 60);
+        s = s % 60;
+        int g = Math.floorDiv(p, 60);
+        p = p % 60;
+        gradi = g;
+        primi = p;
+        secondi = s;
+    }
+
+    public int toSecondi() {
+        int s = 0;
+        s = s + (gradi / 3600) + (primi / 60);
+        return s;
+    }
+
+    public int differenzaSecondi(Angolo a) {
+        return this.toSecondi() - a.toSecondi();
+    }
+
+    public void sommaAngolo(Angolo a) {
+        //todo
+    }
+}
---
+++ Informatica/Java/Angoli/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Angolo a=new Angolo(10,20,10);
+        a.aggiungiGradi(10);
+        a.visualizzaAngolo();
+
+
+    }
+}
