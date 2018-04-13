9e64b1c0af2cb206b278da7e160fa87139682e22
Your Name <you@example.com>
2018-04-13 12:42:41 +0200

    Esercizio finito.

--- Informatica/Java/CdAudio/src/com/company/CD.java
+++ Informatica/Java/CdAudio/src/com/company/CD.java
@@ -1,6 +1,63 @@
 package com.company;
 
 public class CD {
+    String titolo, autore;
+    int numBrani, durata;
 
+    public CD(String titolo, String autore, int numBrani, int durata) {
+        this.titolo = titolo;
+        this.autore = autore;
+        this.numBrani = numBrani;
+        this.durata = durata;
+    }
 
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    public int getNumBrani() {
+        return numBrani;
+    }
+
+    public void setNumBrani(int numBrani) {
+        this.numBrani = numBrani;
+    }
+
+    public int getDurata() {
+        return durata;
+    }
+
+    public void setDurata(int durata) {
+        this.durata = durata;
+    }
+
+    public void compareDurata(CD cd1) {
+        if (this.durata < cd1.durata) {
+            System.out.println(cd1.titolo + "e' piu' lungo di" + this.titolo);
+        } else if (this.durata > cd1.durata) {
+            System.out.println(this.titolo + "e' piu' lungo di" + cd1.titolo);
+        } else System.out.println("I CD hanno uguale durata");
+    }
+
+    @Override
+    public String toString() {
+        return "CD{" +
+                "titolo='" + titolo + '\'' +
+                ", autore='" + autore + '\'' +
+                ", numBrani=" + numBrani +
+                ", durata=" + durata +
+                '}';
+    }
 }
--- Informatica/Java/CdAudio/src/com/company/Main.java
+++ Informatica/Java/CdAudio/src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        CD CD = new CD("Cacca", "Cacco de cacchis", 14, 60);
+        CD CD1 = new CD("Pupù", "Pipì de pupù", 14, 60);
+        System.out.println(CD.toString());
     }
 }
